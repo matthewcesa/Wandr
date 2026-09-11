441917fa07da48c279a76f57ce326dbb1f19d27d
Constant NGUYEN <constant.nguyen@iut-rodez.fr>
2026-09-11 11:04:42 +0200

    Initialisaiton Architecture

---
+++ wandr-backend/src/main/java/wandrbackend/WandrBackendApplication.java
@@ -0,0 +1,13 @@
+package wandrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WandrBackendApplication.class, args);
+    }
+
+}
---
+++ wandr-backend/src/test/java/wandrbackend/WandrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wandrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
